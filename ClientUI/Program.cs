3dcbd25765323dfabbe025c4a9589c1aace52e2c
Brien Smith Martinez <bsmartinez@gmail.com>
2020-03-04 20:27:17 +0100

    Initial commit. PlaceOrder command and PlaceOrder Handler added.

---
+++ ClientUI/Program.cs
@@ -0,0 +1,66 @@
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientUI
+{
+    class Program
+    {
+        static ILog log = LogManager.GetLogger<Program>();
+
+
+        static async Task Main()
+        {
+            Console.Title = "ClientUI";
+
+            var endpointConfiguration = new EndpointConfiguration("ClientUI");
+
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            await RunLoop(endpointInstance).ConfigureAwait(false);
+
+            await endpointInstance.Stop().ConfigureAwait(false);
+        }
+
+        static async Task RunLoop(IEndpointInstance endpointInstance)
+        {
+            while (true)
+            {
+                log.Info("Press 'P' to place an order, or 'Q' to quit.");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+
+                switch (key.Key)
+                {
+                    case ConsoleKey.P:
+                        // Instantiate the command
+                        var command = new PlaceOrder
+                        {
+                            OrderId = Guid.NewGuid().ToString()
+                        };
+
+                        // Send the command to the local endpoint
+                        log.Info($"Sending PlaceOrder command, OrderId = {command.OrderId}");
+                        await endpointInstance.SendLocal(command)
+                            .ConfigureAwait(false);
+
+                        break;
+
+                    case ConsoleKey.Q:
+                        return;
+
+                    default:
+                        log.Info("Unknown input. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+} 
