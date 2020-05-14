f77f32d681d80ab1c82c08482704756390f61bd8
Brien Smith Martinez <bsmartinez@gmail.com>
2020-05-14 18:07:56 +0200

    Add billing project and OrderPlacedHandler

---
+++ Billing/EventHandlers/OrderPlacedHandler.cs
@@ -0,0 +1,22 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus.Logging;
+using Messages.Commands;
+
+namespace Billing.EventHandlers
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlaced>
+    {
+        static ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            log.Info($"Received OrderPlaced, OrderId = {message.OrderId} - Charging credit card...");
+
+            return Task.CompletedTask;
+        }
+    }
+}
