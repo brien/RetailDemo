8d482b2500bde0782d3e1c9698a1894619a7c121
Brien Smith Martinez <bsmartinez@gmail.com>
2020-06-05 18:44:11 +0200

    Add Shipping endpoint and OrderBilledHandler, OrderPlacedHandler

---
+++ Shipping/EventHandlers/OrderBilledHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Messages.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Shipping.EventHandlers
+{
+    public class OrderBilledHandler : IHandleMessages<OrderBilled>
+    {
+        static ILog log = LogManager.GetLogger<OrderBilledHandler>();
+
+        public Task Handle(OrderBilled message, IMessageHandlerContext context)
+        {
+            log.Info($"Received OrderBilled, OrderId = {message.OrderId} - Should we ship now?");
+
+            var orderBilled = new OrderBilled
+            {
+                OrderId = message.OrderId
+            };
+            return context.Publish(orderBilled);
+        }
+    }
+}
---
+++ Shipping/EventHandlers/OrderPlacedHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Messages.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+
+namespace Shipping.EventHandlers
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlaced>
+    {
+        static ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            log.Info($"Received OrderPlaced, OrderId = {message.OrderId} - Should we ship now?");
+
+            var orderPlaced = new OrderBilled
+            {
+                OrderId = message.OrderId
+            };
+            return context.Publish(orderPlaced);
+        }
+    }
+}
